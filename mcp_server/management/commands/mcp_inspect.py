25e6c95730ee73ecada60788f0d5f928916a0945
Zadigo <pendenquejohn@gmail.com>
2026-08-17 19:48:46 +0200

    Add inspect commands

--- mcp_server/management/commands/mcp_inspect.py
+++ mcp_server/management/commands/mcp_inspect.py
@@ -1,54 +1,84 @@
-
 import anyio
 from asgiref.sync import async_to_sync
 from django.core.management.base import BaseCommand
 from mcp import ClientSession
 
-from mcp_server import mcp_server
+from mcp_server.server.base import DJANGO_MCP_SERVER
+
+# async def _check_client():
+#     cl_wr, svr_rd = anyio.create_memory_object_stream(0)
+#     svr_wr, cl_rd = anyio.create_memory_object_stream(0)
+
+#     async def run_server():
+#         await mcp_server._mcp_server.run(
+#             svr_rd,
+#             svr_wr,
+#             mcp_server._mcp_server.create_initialization_options(),
+#         )
+
+#     async def run_client():
+#         async with ClientSession(cl_rd, cl_wr) as session:
+#             await session.initialize()
+
+#             print("Tools discovered in server:")
+#             tool_list = await session.list_tools()
+#             for tool in tool_list.tools:
+#                 print(f'\n\t{tool.name}: {tool.description}')
+#                 print(f'\tParameters: {tool.inputSchema}')
+
+#             print("Resources discovered in server:")
+#             resource_list = await session.list_resources()
+#             for resource in resource_list.resources:
+#                 print(f'\n\t{resource.name}: {resource.description}')
+
+#             print("Prompts discovered in server:")
+#             prompt_list = await session.list_prompts()
+#             for prompt in prompt_list.prompts:
+#                 print(f'\n\t{prompt.name}: {prompt.description}')
+
+#     async with anyio.create_task_group() as tg:
+#         tg.start_soon(run_server)
+#         tg.start_soon(run_client)
 
 
-async def _check_client():
-
-    cl_wr, svr_rd = anyio.create_memory_object_stream(0)
-    svr_wr, cl_rd = anyio.create_memory_object_stream(0)
-
-    async def run_server():
-        await mcp_server._mcp_server.run(
-            svr_rd,
-            svr_wr,
-            mcp_server._mcp_server.create_initialization_options(),
-        )
-
-    async def run_client():
-        async with ClientSession(cl_rd, cl_wr) as session:
-            await session.initialize()
-
-            print("Tools discovered in server:")
-            tool_list = await session.list_tools()
-            for tool in tool_list.tools:
-                print(f'\n\t{tool.name}: {tool.description}')
-                print(f'\tParameters: {tool.inputSchema}')
-
-            print("Resources discovered in server:")
-            resource_list = await session.list_resources()
-            for resource in resource_list.resources:
-                print(f'\n\t{resource.name}: {resource.description}')
-
-            print("Prompts discovered in server:")
-            prompt_list = await session.list_prompts()
-            for prompt in prompt_list.prompts:
-                print(f'\n\t{prompt.name}: {prompt.description}')
-
-    async with anyio.create_task_group() as tg:
-        tg.start_soon(run_server)
-        tg.start_soon(run_client)
-
-
-_check_client_sync = async_to_sync(_check_client)
+# _check_client_sync = async_to_sync(_check_client)
 
 
 class Command(BaseCommand):
     help = 'Inspect installed tools, resources and prompts'
 
     def handle(self, *args, **options):
-        _check_client_sync()
+        async_to_sync(self.inspect())
+
+    async def inspect(self):
+        send_stream, receive_stream = anyio.create_memory_object_stream(0)
+        send_stream1, receive_stream2 = anyio.create_memory_object_stream(0)
+
+        async def runserver():
+            await DJANGO_MCP_SERVER.run(receive_stream, send_stream1, DJANGO_MCP_SERVER._mcp_server.create_initialization_options())
+
+        async def runclient():
+            async with ClientSession(receive_stream2, send_stream) as session:
+                await session.initialize()
+
+                self.stdout.write("Tools discovered in server:")
+
+                result = await session.list_tools()
+                for tool in result.tools:
+                    self.stdout.write(f"  ${self.style.SUCCESS('+')} {tool.name}: {tool.description}")
+                    self.stdout.write(f"      - {tool.input_schema}")
+
+                self.stdout.write("Resources discovered in server:")
+
+                resource_list = await session.list_resources()
+                for resource in resource_list.resources:
+                    self.stdout.write(f'\n\t{resource.name}: {resource.description}')
+
+                print("Prompts discovered in server:")
+                prompt_list = await session.list_prompts()
+                for prompt in prompt_list.prompts:
+                    self.stdout.write(f'\n\t{prompt.name}: {prompt.description}')
+
+        async with anyio.create_task_group() as tg:
+            tg.start_soon(runserver)
+            tg.start_soon(runclient)
--- mcp_server/management/commands/stdio_server.py
+++ mcp_server/management/commands/stdio_server.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand
 
-from mcp_server import mcp_server
+from mcp_server.server.base import DJANGO_MCP_SERVER
 
 
 class Command(BaseCommand):
     help = 'Run the global mcp server over STDIO transport'
 
     def handle(self, *args, **options):
-        mcp_server.run(transport="stdio")
+        DJANGO_MCP_SERVER.run(transport='stdio')
