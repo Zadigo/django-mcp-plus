2ab55b570c18f006531eef8e06bb240bc9ba50c2
Zadigo <pendenquejohn@gmail.com>
2026-08-18 19:29:44 +0200

    Add missing get_text_fields

--- tests/settings.py
+++ tests/settings.py
@@ -27,11 +27,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'mcp_server',
     'tests.testapp',
+    'oauth2_provider',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +129,10 @@ DJANGO_MCP_PLUS_AUTHENTICATION_CLASSES = []
 
 
 DJANGO_MCP_PLUS_AUTHENTICATION_CLASSES = []
+
+
+# Cors settings
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,8 @@
 from django.conf import settings
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 from django.utils.module_loading import import_string
+from oauth2_provider import urls as oauth2_urls
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from mcp_server.views import StreamableHttpView
@@ -12,6 +14,8 @@ permission_classes = [IsAuthenticated] if getattr(settings, 'DJANGO_MCP_PLUS_AUT
 authentication_classes = [import_string(cls) for cls in getattr(settings, 'DJANGO_MCP_PLUS_AUTHENTICATION_CLASSES', [])]
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("o/", include(oauth2_urls)),
     path(
         base_url,
         StreamableHttpView.as_view(
